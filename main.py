69276a6be4c3397a9b0e1f2fef3fc5c361ae642a
lycopersica <77859542+lycopersica@users.noreply.github.com>
2023-10-28 20:25:30 -0500

    whoops

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+import pygame
+import sys
+import math
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Initialize pygame
+pygame.init()
 
+# Set up display
+screen_size = (800, 600)
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption("Multi-Armed Hypnotic Spiral")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Colors
+white = (255, 255, 255)
+black = (0, 0, 0)
 
+clock = pygame.time.Clock()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+running = True
+num_arms = 16  # Number of arms
+angles = [2 * math.pi * i / num_arms for i in range(num_arms)]
+angle_increment = 0.02
+distance_increment = 0.05
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(black)
+
+    for i in range(100):
+        for angle in angles:
+            radius = i * 5
+            x = math.cos(angle + i * angle_increment) * radius + screen_size[0] / 2
+            y = math.sin(angle + i * angle_increment) * radius + screen_size[1] / 2
+            pygame.draw.circle(screen, white, (int(x), int(y)), 5)
+
+    for i in range(len(angles)):
+        angles[i] += angle_increment  # Rotate each arm
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
